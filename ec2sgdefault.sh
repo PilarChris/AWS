78a6f527ddd50a70cc72457001909f3f749067a1
Chri100pher <chri100pher@gmail.com>
2021-06-14 15:36:22 +0200

    Update ec2sgdefault.sh

--- ec2sgdefault.sh
+++ ec2sgdefault.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
 
 for region in `aws ec2 describe-regions --query 'Regions[*].RegionName' --output text`
 do
-	echo "\n '$region':"
-	aws --region $region ec2 describe-instances --filters Name=instance.group-name,Values=default --query 'Reservations[*].Instances[*].[InstanceId]' --output text
+	ec2names=$(aws --region $region ec2 describe-instances --filters Name=instance.group-name,Values=default --query 'Reservations[*].Instances[*].[Tags[?Key == `name`].Value,Tags[?Key == `Name`].Value]' --output text)
+	[  -z "$ec2names" ] && : || printf "$region: \n$ec2names\n"| uniq
 done
